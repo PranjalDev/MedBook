faba9ef8d938ac236ad59b11fadb4fc3b7b1e851
Pranjal Verma <i.pranjal.verma@gmail.com>
2023-10-23 19:12:12 +0530

    remove duplicate constant code.

--- MedBook/DesignConstants/CGFloat+Constants.swift
+++ MedBook/DesignConstants/CGFloat+Constants.swift
@@ -8,9 +8,5 @@
 import Foundation
 
 extension CGFloat {
-    static let headerFont: CGFloat = 32
-    static let textFont: CGFloat = 18
-    static let buttonFont: CGFloat = 22
-    static let titleFont: CGFloat = 28
     static let buttonCornerRadius: CGFloat = 12
 }
